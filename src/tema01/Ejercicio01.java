6cfdbf330d3f172799bd64209eac469fd5095f54
Andres Llontop <andresllontop01@gmail.com>
2022-08-12 13:50:19 -0500

    Se entrega Ejercicios Tema 01

---
+++ src/tema01/Ejercicio01.java
@@ -0,0 +1,17 @@
+package tema01;
+
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        int numeroInt = 1;
+        long numberLong = 11L;
+        double numberDouble = 11.22;
+        boolean numberBoolean = false;
+
+        System.out.println(numeroInt);
+        System.out.println(numberLong);
+        System.out.println(numberDouble);
+        System.out.println(numberBoolean);
+    }
+
+}
