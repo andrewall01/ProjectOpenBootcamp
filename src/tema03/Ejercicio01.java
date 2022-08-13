d6564f84c21937b583d4a93ffce59009b721cd52
Andres Llontop <andresllontop01@gmail.com>
2022-08-12 20:26:43 -0500

    Se entrega Ejercicio del tema 03

---
+++ src/tema03/Ejercicio01.java
@@ -0,0 +1,14 @@
+package tema03;
+
+public class Ejercicio01 {
+
+    public static void main(String [] args){
+        String[] nombres = {"Luis","Roberto","Maria","Luisa"};
+        String[] apellidos = {"Lopez","Paz","Perez","Vidal"};
+
+        for(int i = 0; i<nombres.length;i++){
+            System.out.println("Nombres Completos :"+nombres[i] +" "+ apellidos[i]);
+        }
+    }
+
+}
